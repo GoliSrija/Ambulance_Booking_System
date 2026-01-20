a211d8e06692c17a83b442e3e646bb6e8edf7404
GoliSrija <23251a1241@gmail.com>
2026-01-20 17:30:16 +0530

    Update settings.py

--- AmbulanceBookingSystem/settings.py
+++ AmbulanceBookingSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p@a$*%_3a@qfkw$t7liu=)pu!hvu%3ve$v0pjwce7)&pte%gy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
